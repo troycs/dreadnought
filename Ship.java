155b6b95b67dec60f2cc812ee249ad324cfb5e05
troycs <jtolbert11192@troy.edu>
2009-09-10 16:58:47 -0500

    changes

--- Ship.java
+++ Ship.java
@@ -1,16 +1,32 @@
 /*
 * This class represents a single ship
-*
+* Ships have a certain length which is between 1 and 5 units.
+* A ship with length n has n blocks.
+* Ships have blocks which have the following two states:
+* damaged or undamaged with all blocks undamaged at the start.
+* Ships have an anchor point which describes its position on the grid.
 */
 
 public class Ship
 {
+    //the length of the ship in units
     private int length;
+
+    //the damage state of the ship's blocks
+    private boolean[] damage;
+
+    //the orientation of the ship
     private Orientation orientation;
 
+    //ship constructor
     public Ship(int length)
     {
         this.length = length;
+        //initalize the block damage array
+        this.damage = new boolean[length];
+        //set all blocks to undamaged (false)
+        for (int i=0; i<length; i++)
+            this.damage[i] = false;
         this.orientation = Orientation.HORIZONTAL;
     }
 
